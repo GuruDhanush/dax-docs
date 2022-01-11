7a118f2bc3c2deed6c3d965f51db4cff420dd267
Guru Dhanush <gurudhanush@outlook.com>
2022-01-11 11:54:03 +0530

    Added favicon and web app manifest

--- .eleventy.js
+++ .eleventy.js
@@ -63,6 +63,8 @@ module.exports = function(eleventyConfig) {
   // Copy the `img` and `css` folders to the output
   eleventyConfig.addPassthroughCopy("img");
   eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy("misc");
+
 
   // Customize Markdown library and settings:
   let markdownLibrary = markdownIt({
