61542961877796f1bca8f78378c6eed1c029bb42
Guru Dhanush <gurudhanush@outlook.com>
2022-01-12 11:24:04 +0530

    Added CSP policy

---
+++ _data/metadata.js
@@ -0,0 +1,7 @@
+module.exports = {
+  title: "DAX Docs",
+  url: "http://gurudhanush.github.io/dax-docs",
+  language: "en",
+  description: "For writing documentation related to DAX Docs",
+  env: process.env.ELEVENTY_ENV,
+}
